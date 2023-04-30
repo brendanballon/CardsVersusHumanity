2e07150903cd70375bdb8ed4a8995573dd5fb69d
Brendan Ballon <brendanballon@gmail.com>
2023-04-30 14:47:15 -0400

    Initial Commit

---
+++ Cards vs. Humanity/Cards_vs__HumanityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cards_vs__HumanityApp.swift
+//  Cards vs. Humanity
+//
+//  Created by Brendan Ballon on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Cards_vs__HumanityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
